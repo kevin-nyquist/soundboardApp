2a321bd641e0ce004e30d9f4d741988df1f02360
Kevin N <63832406+kevin-nyquist@users.noreply.github.com>
2023-09-27 00:45:24 -0400

    Updated pages, added public folder and images, and layed out content

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-// Requiring module
-const express = require('express');
- 
+// Requiring modules
+const express = require('express')
+const path = require('path')
+
 // Creating express object
-const app = express();
- 
-// Handling GET request
-app.get('/', (req, res) => {
-    res.send('A simple Node App is '
-        + 'running on this server')
-    res.end()
+const app = express()
+
+// Set up static resources (css files, images, javascript)
+app.use(express.static('./public'))
+
+// Handles error when page is not found
+app.all('*',(req, res) => {
+  res.status(404).send('<h1> Resource not found')
 })
  
 // Port Number
@@ -16,9 +18,4 @@ const PORT = process.env.PORT || 8080;
  
 // Server Setup
 app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
-
-var msg ='hello';
-console.log(msg);
-
-require('soudboard.js');
+  `Server started on port ${PORT}`))
